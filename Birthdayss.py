30f56c1b23cfd691d7bb6ca3ee1f23578bded024
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-09-28 13:56:24 +0200

    gggyr

---
+++ Birthdayss.py
@@ -0,0 +1,10 @@
+print("Birthdays of 4 friends \n")
+#Storing Birthdays in Variables and printing them
+a="12/09/2001"
+print("aakash-",a)
+b="19/08/2000"
+print("\nsigma-",b)
+c="10/11/2004"
+print("\nsigmaboi-",c)
+d="4/01/1997"
+print("\nhabibi-",d)
