6d305d8aa4a8f4dee094c038fd5f749cec337901
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-09-28 13:43:24 +0200

    hiiiiiii

---
+++ variables.py
@@ -0,0 +1,11 @@
+#Creating dfferent variables
+x = 5
+y ="John"
+print(x)
+print(y)
+codingal ="codingal"
+print(codingal)
+
+#User Input
+name = input("enter your name please:  ")
+print("\nHello", name, "\nWelcome to Codingal!!!")
