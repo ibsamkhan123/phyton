1a8a3f1218de3c7280441dc1fbe805d80a75e43f
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-09-28 13:33:52 +0200

    helllloooo

---
+++ activity1.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print ("wecome to the world of programming!")
+#Print a Number
+print(3)
+#next line after print
+print ("hello phyton \n")
+#Pass multiple arguments for printing
+print("hello",5)
+#End argument of print statement
+print("welcome to" , end="*")
