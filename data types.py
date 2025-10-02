ce5f6afb1d304e0089f33506884b738bf9d6f3da
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-10-02 16:10:04 +0200

    sui

---
+++ data types.py
@@ -0,0 +1,23 @@
+#Printing different variables and their data type
+print ("Name:", name)
+print ("Data Type of Name Is", type(name))
+
+print ("Age:", age)
+print ("Data Type of Age Is", type(age))
+
+print ("is student:", is student)
+print ("Data Type of Is student Is", type(is student))
+
+print ("Weight:", weight)
+print ("Data Type of Weight Is", type(weight))
+
+
+#type casting to convert the datatype of variables
+print("\nAfter Typecasting...")
+
+age=str(age)
+print(age)
+print ("Data Type of Age Is", type(age))
+weight=int(weight)
+print (weight)
+print ("Data Type of Weight Is", type(weight))
