cf98cdc14fd8dd48dbd8359af4cd4d05f326ac12
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-10-02 16:18:50 +0200

    whatsuppp

---
+++ string operation.py
@@ -0,0 +1,8 @@
+#input a word
+text=str(input("Enter a string:  "))
+#Reverse string
+#using step value -1 to iterate in reverse
+revText=text[::-1]
+text= revText
+print("Reverse of given string is:")
+print(text)
