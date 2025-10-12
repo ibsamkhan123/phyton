7a4211b1e045a8e301c05923efe7d224a894c73e
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-10-12 13:31:14 +0200

    andandor

---
+++ odd even.py
@@ -0,0 +1,6 @@
+number =int(input("Enter a number to check"))
+print("Number to be checked:", number)
+if number%2==0 :
+    print("this is an even number")
+    else
+print("this is an odd number")
