b65d9418441eccf74ed2746a3acdf2a6d8d34ea8
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-10-30 14:59:08 +0100

    miiiiiiiiiiiiiiiiiiiii

---
+++ Loop.py
@@ -0,0 +1,11 @@
+#Input the values of terms
+n = int(input("Enter the value of terms:  "))
+
+
+sum = 0 #intialise
+i = 1 #intialise
+while i<=n: #Loop will run from 1 to n
+    sum = sum+i
+    i = i+1 
+
+print("\nSum =", sum)
