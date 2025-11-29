707940ca0a090f7903c09df2615df49e12e3eb92
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-11-29 10:40:03 +0100

    uo

---
+++ Continue.py
@@ -0,0 +1,8 @@
+var = 10        #intialise
+while var > 0:   #literate loop
+    var = var- 1
+    if var == 5:     #condition one
+       continue      #continue statement
+    #display result
+    print('\nCurrent variable value: ', var)
+print('\nGoodbye!')
