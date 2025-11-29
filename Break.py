075f997d17048ff226b0e0bc84746bcece876886
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-11-29 10:21:39 +0100

    roki

---
+++ Break.py
@@ -0,0 +1,12 @@
+#take user input
+a = input ("Enter the Word:  ")
+#program to check break keyword
+for i in a:#literate for loop
+    if (i == 'a'):#condition 1
+        #display result
+         print("a is found")
+         break #break statement
+    else:
+    #display result
+     print("a is not found")ibsam
+     
