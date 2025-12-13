e89669c4b2f26c55ef145bafcc24235c41045f52
ibsamkhan123 <ibsamkhan1@gmail.com>
2025-12-13 13:14:08 +0100

    NARLI

---
+++ Trip.py
@@ -0,0 +1,8 @@
+#Define a function called hotel_host with one argument nights as input
+def hotel_cost(nights):
+    return 140*nights
+
+#define a function called plane_ride_cost that takes a string, city, as input.
+def plane_ride_cost(city):
+if "Rome" == city:
+    return 900
