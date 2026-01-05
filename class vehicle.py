5ceea68ccdde355c88ade606408f58e960f004b0
ibsamkhan123 <ibsamkhan1@gmail.com>
2026-01-05 12:57:32 +0100

    jiuce

---
+++ class vehicle.py
@@ -0,0 +1,15 @@
+#create class
+class vehicle:
+
+    #create init method
+   def __init__(self, max_speed, mileage):
+              #bind the arguments
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+#Object creation
+modelX = vehicle(240, 18)
+
+#access the variables inside init method
+print("Model max speed:", modelX.max_speed)
+print("Model Mileage",modelX.mileage)
